c0d792151e90f1c99c2b8675ab86f4ac255f291d
ferswift <nandofhen@gmail.com>
2025-03-12 15:20:34 -0300

    Removido botão e adicionado ancora

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,3 @@
-import { Button } from "../components/Button";
-
 export const Card = ({
   reverse = false,
   image,
@@ -24,16 +22,14 @@ export const Card = ({
         <div className="flex flex-col justify-center items-center max-w-[30rem] gap-5 text-1xl">
           <p className="text-slate-600">{description}</p>
           <p className="text-slate-400">{secondaryDescription}</p>
-          <Button
-            bgColor="bg-blue-600 hover:bg-blue-700"
-            textColor="text-white"
-            shadow="shadow-[0_10px_20px_rgba(37,99,235,0.5),0_6px_6px_rgba(37,99,235,0.5),0_0_100px_-10px_rgba(37,99,235,0.5)]"
-            className="w-[200px] h-[50px] z-10"
+          <a
+            href={githubLink}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="inline-block font-semibold no-underline tracking-tight py-2 px-4 rounded-lg transition-transform duration-300 ease-in-out hover:translate-y-[-4px] bg-blue-700 text-slate-100 shadow-[0_10px_20px_rgba(0,0,255,0.5),0_6px_6px_rgba(0,0,255,0.5),0_0_100px_-10px_rgba(0,0,255,0.5)]"
           >
-            <a href={githubLink} target="_blank" rel="noopener noreferrer">
-              GitHub
-            </a>
-          </Button>
+            GitHub
+          </a>
         </div>
       </div>
     </div>
